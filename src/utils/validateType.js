cef1ad28f2c70e4e1a1e66cec351165a26b2ac2e
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-05-16 14:41:56 -0300

    refactor: update validation of type

--- src/utils/validateType.js
+++ src/utils/validateType.js
@@ -2,14 +2,27 @@ const Exception = require('../middlewares/Exception')
 
 function validateType(param, type, field) {
   if (type == 'number') {
-    param = parseInt(param)
+    param = parseInt(param);
+    
+    if (typeof param == type && !isNaN(param)) {
+      return param;
+    }
   }
 
-  if(typeof param != type || (type=='number' && isNaN(param))) {
-    throw new Exception(`The field '${field}' must be of type '${type}'`);
+  if (type == 'email') {
+    const emailRE = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/g
+
+    if (param.match(emailRE)) {
+      return param;
+    }
   }
 
-  return param;
+  if (type == 'string') {
+    return param;
+  }
+
+  throw new Exception(`The field '${field}' must be of type '${type}'`);
+
 }
 
 module.exports = validateType;
