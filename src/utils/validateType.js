1c1b92d11ca1a7c1b139f9a907cd93f340bf1aa6
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-05-16 22:05:26 -0300

    fix: email regex

--- src/utils/validateType.js
+++ src/utils/validateType.js
@@ -13,7 +13,7 @@ function validateType(param, type, field) {
   }
 
   if (Vtype === 'email') {
-    const emailRE = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/g;
+    const emailRE = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
 
     if (Vparam.match(emailRE)) {
       return Vparam;
