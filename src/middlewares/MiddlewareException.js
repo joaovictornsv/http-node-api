75d816f0f74b7f1f1f231e752f65601775a0e96f
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-05-20 20:04:46 -0300

    fix: middleware return allow origin

--- src/middlewares/MiddlewareException.js
+++ src/middlewares/MiddlewareException.js
@@ -2,6 +2,8 @@ const Exception = require('./Exception');
 
 class MiddlewareException {
   handle(res, error) {
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,PUT,DELETE');
     if (error instanceof Exception) {
       res.writeHead(error.statusCode, error.contentType);
       res.write(error.message);
